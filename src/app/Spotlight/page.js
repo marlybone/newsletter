6d2c067634828a40c81db98e9a15a4b2c82a3429
Marlon Stevenson <marlybone@gmail.com>
2024-02-14 21:00:09 +0100

    updated navbar css slightly

--- src/app/Spotlight/page.js
+++ src/app/Spotlight/page.js
@@ -6,14 +6,24 @@ export default function SpotlightPage() {
     <>
     <div class="relative">
   <img src="sapphire.jpg" className="absolute inset-0 w-full h-80 object-cover" alt="Background Image" />
-  <div class="relative text-[#333] font-[sans-serif] mb-10 min-h-screen p-4 ">
-    <div class="max-w-5xl mx-auto text-center">
-      <div class="max-w-lg mx-auto bg-gray-100 flex px-2 py-1 rounded-full text-left mt-44 border focus-within:border-gray-700">
-        <input type='email' placeholder='Search' class="w-full bg-transparent text-sm px-4 py-3" />
+  <div className="relative text-[#333] font-[sans-serif] mb-10 min-h-screen p-4 ">
+    <div clasNames="max-w-5xl mx-auto text-center">
+      <div className="max-w-lg mx-auto bg-gray-100 flex px-2 py-1 rounded-full text-left mt-44 border focus-within:border-gray-700">
+        <input type='email' placeholder='Search' className="w-full bg-transparent text-sm px-4 py-3" />
       </div>
     </div>
   </div>
 </div>
+<section>
+  <div className="flex">
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+  </div>
+</section>
     </>
   );
 };
