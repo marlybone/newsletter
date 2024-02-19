1869d11d10bb173d247d4e1e3410bc201be9acd2
Marlon Stevenson <marlybone@gmail.com>
2024-02-19 11:57:22 +0100

    search query componant

---
+++ src/app/components/search.js
@@ -0,0 +1,12 @@
+"use client"
+import React from "react"
+import { useState, useEffect } from 'react'
+
+export default createSearchQuery = () => {
+const [searchQuery, setSearchQuery] = useState('')
+const [searchResults, setSearchResults] = useState([]);
+const [showSearchResults, setShowSearchResults] = useState(false);
+
+
+
+}
