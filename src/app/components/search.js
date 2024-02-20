6374d44f14e295f7617f8f53e7821f1d654a2cff
Marlon Stevenson <marlybone@gmail.com>
2024-02-20 15:02:13 +0100

    crafting groq query

--- src/app/components/search.js
+++ src/app/components/search.js
@@ -1,12 +1,18 @@
 "use client"
 import React from "react"
 import { useState, useEffect } from 'react'
+import client from '@sanity/sanity.client'
 
 export default createSearchQuery = () => {
 const [searchQuery, setSearchQuery] = useState('')
 const [searchResults, setSearchResults] = useState([]);
 const [showSearchResults, setShowSearchResults] = useState(false);
 
+const handleSearch = async (query) => {
+    const searchQuery = await client.fetch(
+        `*[_type == 'post' && (title match "${query}" || content match "${query}")]`
+    )
+}
 
 
 }
