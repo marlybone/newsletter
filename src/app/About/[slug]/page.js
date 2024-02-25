56c233c4e847e2004105b10145e0884b9b06206d
Marlon Stevenson <marlybone@gmail.com>
2024-02-25 20:28:40 +0100

    some mobile view design

--- src/app/About/[slug]/page.js
+++ src/app/About/[slug]/page.js
@@ -14,11 +14,11 @@ const [showAuthor, setShowAuthor] = ('')
         </div>
       </section>
       <section className='mt-16'>
-        <div className=''>
-            <div className='md:flex-row md:flex space-x-4 justify-center'>
-                <img className='shadow-custom rounded-2xl justify-start h-96 w-96' src="/bgimg.jpg" />
+        <div className='items-center text-center md:text-start'>
+            <div className='md:flex-row flex space-x-4 flex-col items-center space-y-4'>
+                <img className='shadow-custom rounded-2xl flex justify-center justify-items-center h-96 w-96' src="/bgimg.jpg" />
                 <div className='self-center flex-col'>
-                    <h1>Marlon Stevenson</h1>
+                    <h1 className=''>Marlon Stevenson</h1>
                     <p>I'm  a self-taught developer, tech enthusiast, and avid learner with a deep interest in science, technology, and games. His curiosity and passion for learning have led him to create this site as a platform to gain insights from individuals who have successfully transitioned to new careers, especially in the tech industry. Marlon is dedicated to continuous growth and self-improvement, and he believes that sharing experiences and learning from others is a valuable way to achieve personal and professional development</p>
                 </div>
             </div>
