d66d42b2e2a7ae61826be3a0ecb9e88aa9d362bc
Marlon Stevenson <marlybone@gmail.com>
2024-02-26 17:20:35 +0100

    adding moving border framer motion

--- src/app/About/[slug]/page.js
+++ src/app/About/[slug]/page.js
@@ -1,29 +1,49 @@
 "use client"
-import React from 'react';
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
+import { Button } from "../../components/moving-border";
 
 export default function About() {
-const [showAuthor, setShowAuthor] = ('')
+    const [activeButton, setActiveButton] = useState(null);
+
+    const handleButtonClick = (buttonId) => {
+      setActiveButton(buttonId);
+    };
 
   return (
     <div className="container mt-44 min-h-screen max-w-5xl mx-auto">
       <section>
-        <div className='flex flex-col items-center'>
-            <img className='rounded-full w-48 h-48 shadow-custom' src='/mainimg.jpg' />
-            <h1>Founder & Author</h1>
+        <div className="flex flex-row items-center justify-center space-x-4">
+    <Button/>
+
+
         </div>
       </section>
-      <section className='mt-24'>
-        <div className='items-center text-center md:text-start'>
-            <div className='md:flex-row flex space-x-4 flex-col items-center space-y-4'>
-                <img className='shadow-custom rounded-2xl flex justify-center justify-items-center h-96 w-96' src="/bgimg.jpg" />
-                <div className='self-center flex-col'>
-                    <h1 className=''>Marlon Stevenson</h1>
-                    <p>I'm  a self-taught developer, tech enthusiast, and avid learner with a deep interest in science, technology, and games. His curiosity and passion for learning have led him to create this site as a platform to gain insights from individuals who have successfully transitioned to new careers, especially in the tech industry. Marlon is dedicated to continuous growth and self-improvement, and he believes that sharing experiences and learning from others is a valuable way to achieve personal and professional development</p>
-                </div>
+      <section className="mt-24">
+        <div className="items-center text-center md:text-start">
+          <div className="md:flex-row flex space-x-4 flex-col items-center space-y-4">
+            <img
+              className="shadow-custom rounded-2xl flex justify-center justify-items-center h-96 w-96"
+              src="/bgimg.jpg"
+              alt="Background image"
+            />
+            <div className="self-center flex-col">
+              <h1>Marlon Stevenson</h1>
+              <p>
+                I'm a self-taught developer, tech enthusiast, and avid learner
+                with a deep interest in science, technology, and games. His
+                curiosity and passion for learning have led him to create this
+                site as a platform to gain insights from individuals who have
+                successfully transitioned to new careers, especially in the tech
+                industry. Marlon is dedicated to continuous growth and
+                self-improvement, and he believes that sharing experiences and
+                learning from others is a valuable way to achieve personal and
+                professional development.
+              </p>
             </div>
+          </div>
         </div>
       </section>
     </div>
   );
-};
+}
---
+++ src/app/components/moving-border.js
@@ -0,0 +1,147 @@
+"use client";
+import React, { useState } from "react";
+import {
+  motion,
+  useAnimationFrame,
+  useMotionTemplate,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import { useRef } from "react";
+import { cn } from "../../../utils/cn";
+
+const authors = [
+    {url: "/mainimg.jpg", name: "Marlon Stevenson", color: "h-20 w-20 opacity-[0.9] bg-[radial-gradient(var(--fuchsia-600)_40%,transparent_60%)]"},
+    {url: "/bgimg.jpg", name: "Carlos Rimba", color: "h-20 w-20 opacity-[0.9] bg-[radial-gradient(var(--sky-600)_40%,transparent_60%)]"}
+];
+ 
+export function Button({
+  borderRadius = "5rem",
+  children,
+  as: Component = "button",
+  containerClassName,
+  borderClassName,
+  duration,
+  className,
+  ...otherProps
+}) {
+    const [selectedTab, setSelectedTab] = useState(authors[0]);
+    return (
+        <>
+          {authors.map((item) => (
+            <Component
+              className={cn(
+                "shadow-custom relative h-32 w-32 rounded-full p-[3px] overflow-hidden",
+                containerClassName
+              )}
+              style={{
+                borderRadius: borderRadius,
+              }}
+              {...otherProps}
+            >
+              <div
+                className="absolute inset-0"
+                style={{ borderRadius: `calc(${borderRadius} * 0.96)` }}
+              >
+                <MovingBorder
+                  duration={duration}
+                  rx="30%"
+                  ry="30%"
+                  isActive={item === selectedTab}
+                >
+                  <div
+                    className={cn(
+                      `${item.color}`,
+                    )}
+                  />
+                </MovingBorder>
+              </div>
+    
+              <div
+                className={cn(
+                  "relative backdrop-blur-xl flex items-center justify-center w-full h-full text-sm antialiased",
+                  className
+                )}
+                style={{
+                  borderRadius: `calc(${borderRadius} * 0.96)`,
+                }}
+              >
+                <img
+                  key={item.name}
+                  src={item.url}
+                  onClick={() => {
+                    setSelectedTab(item);
+                  }}
+                  className={`${item === selectedTab ? "selected" : ""} h-22 w-22 rounded-full`}
+                />
+              </div>
+            </Component>
+          ))}
+        </>
+      );
+    }
+ 
+export const MovingBorder = ({
+  children,
+  duration = 2000,
+  rx,
+  ry,
+  isActive,
+  ...otherProps
+}) => {
+  const pathRef = useRef();
+  const progress = useMotionValue(0);
+  useAnimationFrame((time) => {
+    if (!isActive) return; // Pause animation if isActive is false
+
+    const length = pathRef.current?.getTotalLength();
+    if (length) {
+      const pxPerMillisecond = length / duration;
+      progress.set((time * pxPerMillisecond) % length);
+    }
+  });
+ 
+  const x = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).x
+  );
+  const y = useTransform(
+    progress,
+    (val) => pathRef.current?.getPointAtLength(val).y
+  );
+ 
+  const transform = useMotionTemplate`translateX(${x}px) translateY(${y}px) translateX(-50%) translateY(-50%)`;
+ 
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        className="absolute h-full w-full"
+        width="100%"
+        height="100%"
+        {...otherProps}
+      >
+        <rect
+          fill="none"
+          width="100%"
+          height="100%"
+          rx={rx}
+          ry={ry}
+          ref={pathRef}
+        />
+      </svg>
+      <motion.div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          display: "inline-block",
+          transform,
+        }}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+};
