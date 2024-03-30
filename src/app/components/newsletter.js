4188b9c203c82104c1db7b226b417306373bf425
git config --global user.name Marlon <marlybone@gmail.com>
2024-03-30 16:54:16 +0100

    custom fonts and animation

--- src/app/components/newsletter.js
+++ src/app/components/newsletter.js
@@ -1,15 +1,16 @@
 import React from "react";
+import styles from './moving-border.module.css'
 
 export default function Newsletter() {
   return (
     <div className="bg-white text-[#333] font-[sans-serif] min-h-screen items-center self-center flex rounded-lg">
       <div className="max-w-5xl md:mx-auto text-center border-px shadow-custom rounded-xl py-12 p-4 mx-2 relative overflow-hidden">
-      <img className="absolute inset-0 bg-cover z-0 w-full h-32" src="./slice.png"/>
+      <img className="absolute inset-0 bg-cover z-0 w-full h-40" src="./slice.png"/>
       <div className="z-10 absolute top-0 -inset-0 mt-8">
-        <h1 className="text-5xl font-extrabold text-white">Explore</h1>
+        <h1 className={`${styles.luminosity} ${styles.Monoton}`}>E<span className={styles.flickerfast}>x</span>pl<span className={styles.flickerslow}>o</span>re</h1>
         </div>
         <div className="flex align-middle items-end md:mt-16 mt-24">
-        <p className="text-sm md:mx-12 mt-20">
+        <p className="text-lg md:mx-12 mt-20">
           Subscribe to our newsletter and stay up to date with the latest news,
           updates, and exclusive offers. Get valuable insights. Join our
           community today!
