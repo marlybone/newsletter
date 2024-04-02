41ccb1706f8fb1749ffb05a65d797d28c0f108b7
git config --global user.name Marlon <marlybone@gmail.com>
2024-04-02 21:52:31 +0200

    hero page complete

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -5,9 +5,16 @@ export default function Hero() {
   return (
     <BackgroundGradientAnimation>
     <div className="absolute z-50 inset-0 flex justify-start content-start mx-auto max-w-7xl text-white px-4 pointer-events-none text-3xl text-center">
-      <div className="flex mt-32 drop-shadow-2xl flex-col space-y-2">
-        <h1 className="md:text-8xl text-5xl flex justify-start">Discover</h1>
-        <p className="md:text-2xl text-xl flex justify-start ml-1">Stories from Real People</p>
+      <div className="flex mt-32 drop-shadow-2xl flex-col space-y-3">
+        <h1 className="md:text-8xl text-5xl flex justify-start font-bold text-left">Discover</h1>
+        <p className="md:text-2xl text-xl flex justify-start text-left">Stories from Real People</p>
+        <p className="text-lg w-1/3 text-left">
+        We cast a spotlight on the bold individuals who've navigated career changes, showcasing their journeys of resilience, determination, and self-discovery.
+        </p>
+        <button className="border text-sm font-medium relative border-neutral-200 text-white dark:text-white px-4 py-2 rounded-full w-24">
+          <span>Subscribe</span>
+          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
+        </button>
       </div>
     </div>
   </BackgroundGradientAnimation>
