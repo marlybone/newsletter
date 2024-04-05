baabd830be4487792934468cd0d1f8eccbd1aa47
git config --global user.name Marlon <marlybone@gmail.com>
2024-04-05 23:50:32 +0200

    commit

--- src/app/components/contact.js
+++ src/app/components/contact.js
@@ -1,10 +1,14 @@
 "use client"
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 export default function ContactForm() {
 
     return (
-        <></>
+        <>
+        <div className='max-w-5xl container my-12 flex justify-start items-center  mx-auto'>
+        Hi    
+        </div>
+        </>
         )
 }
