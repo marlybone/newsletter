5cefdbb3c5c8b7d18b8fb64d75b1bb0070ca6a22
Marlon <marlybone@gmail.com>
2024-05-20 23:09:20 +0200

    button fires on success of email

--- src/app/components/contactus.js
+++ src/app/components/contactus.js
@@ -4,45 +4,46 @@ import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import { useForm } from 'react-hook-form';
 import styles from "./moving-border.module.css"
-import { motion, AnimatePresence, useAnimationControls } from "framer-motion";
-
+import { motion, AnimatePresence, easeIn } from "framer-motion";
 
 export default function ContactUs() {
   const [isSuccess, setIsSuccess] = useState(false);
-
   const {
       register,
       formState: {errors},
   } = useForm();
     const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDedault();
-    setIsSuccess(true)
-
-    emailjs
-      .sendForm('service_9pl8maaff', 'template_junwsgp', form.current, {
-        publicKey: 'QOufJT4a6l-SjdOFP',
-      })
-      .then(
-        () => {
-          setIsSuccess(true)
-
-          setTimeout(() => {
-            setIsSuccess(false);
-          }, 3000)
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  }
-
-
+    const sendEmail = (e) => {
+      e.preventDefault();
+                  setIsSuccess(true)
+            console.log("true")
+            setTimeout(() => {
+              setIsSuccess(false);
+            }, 3000)
+  
+      // emailjs
+      //   .sendForm('service_9pl8maa', 'template_junwsgp', form.current, {
+      //     publicKey: 'QOufJT4a6l-SjdOFP',
+      //   })
+      //   .then(
+      //     () => {
+      //       setIsSuccess(true)
+      //       console.log("true")
+      //       setTimeout(() => {
+      //         setIsSuccess(false);
+      //       }, 3000)
+      //     },
+      //     (error) => {
+      //       console.log('FAILED...', error.text);
+      //     },
+      //   );
+    }
+  
 
     return (
       <div className='p-2 h-full md:mt-6 w-full flex'>
-        <form className='flex flex-col h-full w-full mt-2 justify-start items-center space-y-8' ref={form} >
+        <form className='flex flex-col h-full w-full mt-2 justify-start items-center space-y-8' ref={form} onSubmit={sendEmail}>
              <div className='relative'>
      <input
             {...register("firstName", { required: "This Is Required", minLength: {
@@ -81,33 +82,15 @@ export default function ContactUs() {
         <span id={styles.bar}/>
         <label htmlFor="boxInput" id="label" className={styles.label}>Message</label>
         </div>
-        <Button/>
+        <Button isSuccess={isSuccess}/>
         </form>
         </div>
         )
 }
 
 const Button = ({
+  isSuccess
 }) => {
-  const [isSuccess, setIsSuccess] = useState(false);
-  const controlsDiv = useAnimationControls()
-
-  const showSign = () => {
-    setIsSuccess(true)
-  }
-
-  useEffect(() => {
-    const sequence = async () => {
-      controlsDiv.set("hidden")
-      await controlsDiv.start("visible");
-    }
-    if (isSuccess) {
-      sequence()
-      setTimeout(() => {
-        setIsSuccess(false)
-      }, 3000)
-    }
-  })
 
   const variants = {
     hidden: {
@@ -125,35 +108,42 @@ const Button = ({
     } 
   }
 
+
   return (
   <AnimatePresence>
-  <motion.div 
-  className='h-full flex items-end self-center p-4 relative'>
+  <div 
+  className='h-full flex items-end self-center p-4 relative bg-white'>
   <motion.button 
+  type="submit"
   animate={{backgroundColor: isSuccess ? "#388e3c" : "#52525B"}}
   transition={{duration: "1"}}
-  className={`${styles.submitButton} justify-center items-center relative`} onClick={(showSign)}>
+  className={`justify-center items-center relative outline-none overflow-hidden border-none h-[50px] w-[220px] text-white text-[22px] text-center rounded-full shadow-md shadow-[-1px_6px_10px_2px rgba(0, 0, 0, 0.2)]`}>
       <motion.p 
       animate={{x : isSuccess ? "-40px" : "0px"}}
       transition={{ duration: "1" }}
       className="flex justify-center align-middle p-1 text-lg">{isSuccess ? "Thanks" : "Submit"}</motion.p>
       <motion.div
       className={`${styles.checkBox} justify-center flex items-center p-2`}
-      animate={{ opacity: isSuccess ? "1" : "0", right: isSuccess ? "0px" : "-45px" }}
+      animate={{ opacity: isSuccess ? "1" : "0", right: isSuccess ? "0px" : "-40px" }}
       transition={{ duration: "1"}}
       >
-<motion.svg 
-variants={variants}
-animate={controlsDiv}
-width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
-<motion.path 
-variants={variants}
-animate={controlsDiv}
-d="M5 12l5 5l10 -10" />
-</motion.svg>
+    {isSuccess && (
+    <motion.svg
+    initial="hidden"
+    animate="visible"
+    variants={variants}
+    width="24"  height="24"  viewBox="0 0 24 24"  fill="none" stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">
+    <motion.path 
+    initial="hidden"
+    animate="visible"
+    variants={variants}
+    d="M5 12l5 5l10 -10"
+    />
+    </motion.svg>
+    )}
     </motion.div>
   </motion.button>
-</motion.div>
+</div>
 </AnimatePresence>
   )
 }
